e42ded923ad6a1af3ab23e4bb6da50f1cae612c2
cromega <cr.jrg08@gmail.com>
2022-11-28 10:50:45 -0500

    greetingsSpring

---
+++ src/main/java/app/spring/greetingsSpring/Application.java
@@ -0,0 +1,25 @@
+package app.spring.greetingsSpring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {SpringApplication.run(Application.class, args);}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context) {
+		return args -> {
+			System.out.println("-> Inspecting Beans provided");
+			String[] beanNames = context.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName: beanNames) {System.out.println(beanName);}
+		};
+	}
+}
